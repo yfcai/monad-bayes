8922863b12217cc444fb05bfc93d287e3ebdebb3
Adam Scibior <ams240@cam.ac.uk>
2015-10-28 11:19:07 +0100

    Redesigned JDist to make eval and density possible

--- src/Dist.hs
+++ src/Dist.hs
@@ -15,6 +15,7 @@ import System.Random
 import Control.Applicative (Applicative, pure, (<*>))
 import Control.Arrow (first, second)
 import Control.Monad (liftM, liftM2)
+import Data.Proxy
 
 import qualified Data.Random as Ext
 import qualified Data.Random.Distribution.Bernoulli as Bern
@@ -111,47 +112,49 @@ instance Sampleable Dist where
     sample g (Conditional c d) = error "Attempted to sample from a conditional distribution."
 
 
-data JDist :: * -> * -> * -> * where
-    JReturn :: a -> JDist x x a
-    JBind :: JDist x y a -> (a -> JDist y z b) -> JDist  x z b
+data JDist :: * -> * -> * where
+    JReturn :: a -> JDist (HList '[])  a
+    JBind :: (HSplitAt n zs xs ys) => Proxy n ->
+             JDist (HList xs) a -> (a -> JDist (HList ys) b) ->
+             JDist (HList zs) b
     JPrimitive :: (Ext.Distribution d a, Ext.PDF d a) =>
-                  d a -> JDist (HList x) (HList (a ': x)) a
-    JConditional :: (a -> Prob) -> JDist x y a -> JDist x y a
+                  d a -> JDist (HList (a ': '[])) a
+    JConditional :: (a -> Prob) -> JDist x a -> JDist x a
 
-instance Functor (JDist x y) where
-    fmap f d = d `JBind` (JReturn . f)
-
-instance IxFunctor JDist where
-    imap = fmap
-
-instance IxPointed JDist where
-    ireturn = JReturn
-
-instance IxApplicative JDist where
-    iap f d = f `JBind` \f -> d `JBind` (ireturn . f)
-
-instance IxMonad JDist where
-    ibind = flip JBind
-
-instance Bernoulli (JDist (HList x) (HList (Bool ': x))) where
+instance Bernoulli (JDist (HList (Bool ': '[]))) where
     bernoulli p = JPrimitive $ Bern.Bernoulli $ toDouble p
 
-instance Normal (JDist (HList x) (HList (Double ': x))) where
+instance Normal (JDist (HList (Double ': '[]))) where
     normal m s = JPrimitive $ Ext.Normal m s
 
-instance Conditional (JDist x y) where
+instance Conditional (JDist x) where
     condition = JConditional
 
-instance Sampleable (JDist x y) where
+instance Sampleable (JDist x) where
     sample g (JReturn x) = x
-    sample g (JBind d f) = sample g1 $ f $ sample g2 d where
-                                (g1,g2) = split g
+    sample g (JBind _ d f) = sample g1 $ f $ sample g2 d where
+                              (g1,g2) = split g
     sample g (JPrimitive d) = fst $ Ext.sampleState d g
     sample g (JConditional c d) = error "Attempted to sample from a conditional distribution."
- 
 
-marginal :: JDist x y a -> Dist a
+eval :: JDist (HList x) a -> HList x -> a
+eval (JReturn a) _ = a
+eval (JBind n d f) xs = f (eval d xs1) `eval` xs2
+    where (xs1,xs2) = hSplitAt n xs 
+eval (JPrimitive d) xs = hHead xs
+eval (JConditional c d) xs = eval d xs
+
+
+density :: JDist (HList x) a -> HList x -> Prob
+density (JReturn _) _  = 1
+density (JBind n d f) xs = density d xs1 * density (f x) xs2 where
+    (xs1,xs2) = hSplitAt n xs
+    x = eval d xs1
+density (JPrimitive d) xs = prob $ Ext.pdf d (hHead xs)
+density (JConditional c d) xs = c (eval d xs) * density d xs
+
+marginal :: JDist x a -> Dist a
 marginal (JReturn x) = return x
-marginal (JBind d f) = marginal d >>= (marginal . f)
+marginal (JBind _ d f) = marginal d >>= (marginal . f)
 marginal (JPrimitive d) = external d
 marginal (JConditional c d) = Conditional c (marginal d)
