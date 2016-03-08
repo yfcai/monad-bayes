317c08c0fcc885ad9c09c03842c14a28270b213f
Yufei Cai <yufei.cai@uni-tuebingen.de>
2016-03-08 16:06:01 +0100

    add criterion's helloworld fibber benchmark

--- models/BenchAll.hs
+++ models/BenchAll.hs
@@ -4,5 +4,26 @@ import qualified DPmixture
 import qualified Gamma
 import qualified HMM
 
+import Criterion.Main
+import Criterion.Main.Options
+import Criterion.Types
+
+myDefaultConfig = defaultConfig
+  { timeLimit = 0.1 -- run long-running benchmarks only once by default
+  }
+
+
+
 main :: IO ()
-main = putStrLn "Benchmark not yet implemented."
+main = defaultMainWith myDefaultConfig [
+  bgroup "fib" [ bench "x"  $ whnf fib 33
+               ]
+  ]
+
+fib :: Integer -> Integer
+fib m | m < 0     = error "negative!"
+      | otherwise = go m
+  where
+    go 0 = 0
+    go 1 = 1
+    go n = go (n-1) + go (n-2)
