yfcai/monad-bayes

commit 2928606090802feb908190d536cfaa5b95375c81

never start with infeasible sample
While asymptotically insignificant, the initial infeasible
samples causes KL divergence to be infinite.

src/Inference.hs
@@ -79,10 +79,12 @@ mh n init trans = evalStateT (start >>= chain n) 1 where
   -- start :: StateT LogFloat m a
   start = do
     (x, p) <- lift $ runWeightedT init
-    put p
-    return x
+    if p == 0 then
+      start
+    else
+      return x
 
-  --chain :: Int -> StateT LogFloat m a -> StateT LogFloat m [a]
+  --chain :: Int -> a -> StateT LogFloat m [a]
   chain 0 _ = return []
   chain n x = do
     p <- get

commit 28a0e8efe8aaa92163c7c1218e8f1c0e981907ea

fix bug in MH: Did not save weight of initial RandomDB
Introduced in 2928606090802feb908190d536cfaa5b95375c81

src/Inference.hs
@@ -81,7 +81,7 @@ mh n init trans = evalStateT (start >>= chain n) 1 where
     if p == 0 then
       start
     else
-      return x
+      put p >> return x
 
   --chain :: Int -> a -> StateT LogFloat m [a]
   chain 0 _ = return []